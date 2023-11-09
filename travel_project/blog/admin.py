cab3c5b7d1bcdf297e69e2b28f5fbc8682dea3a7
florivillar <149096386+florivillar@users.noreply.github.com>
2023-11-09 12:41:16 -0300

    Add files via upload

---
+++ travel_project/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from blog.models import Post, Destino, Comunidad
+
+# Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Destino)
+admin.site.register(Comunidad)
