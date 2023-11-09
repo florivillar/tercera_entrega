f978f4cb1d7015f6a808d88383eb2d8d6e6f83c7
florivillar <149096386+florivillar@users.noreply.github.com>
2023-11-09 14:35:10 -0300

    Add files via upload

---
+++ travel_project/blog/views.py
@@ -0,0 +1,73 @@
+# Create your views here.
+
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.db.models import Q
+from .forms import PostForm, DestinoForm, ComunidadForm
+from .models import Post, Destino, Comunidad
+
+def search_results(request):
+    query = request.GET.get('q')
+    results = {}
+
+    if query:
+        post_results = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query))
+        destino_results = Destino.objects.filter(Q(nombre__icontains=query))
+        comunidad_results = Comunidad.objects.filter(Q(nombre__icontains=query))
+
+        results = {
+            'posts': post_results,
+            'destinos': destino_results,
+            'comunidades': comunidad_results,
+        }
+
+    return render(request, 'search_results.html', {'results': results, 'query': query})
+
+def search_form(request):
+    return render(request, 'search_form.html')
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def lista_posts(request):
+    posts = Post.objects.all()
+    return render(request, 'lista_posts.html', {'posts': posts})
+
+def crear_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_posts')
+    else:
+        form = PostForm()
+    return render(request, 'crear_post.html', {'form': form})
+
+def lista_destinos(request):
+    destinos = Destino.objects.all()
+    return render(request, 'lista_destinos.html', {'destinos': destinos})
+
+def crear_destino(request):
+    if request.method == "POST":
+        form = DestinoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_destinos')
+    else:
+        form = DestinoForm()
+    return render(request, 'crear_destino.html', {'form': form})
+
+def lista_comunidades(request):
+    comunidades = Comunidad.objects.all()
+    return render(request, 'lista_comunidades.html', {'comunidades': comunidades})
+
+def crear_comunidad(request):
+    if request.method == "POST":
+        form = ComunidadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_comunidades')
+    else:
+        form = ComunidadForm()
+    return render(request, 'crear_comunidad.html', {'form': form})
+
